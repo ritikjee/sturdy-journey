62af3d776ee350223000129898ed1bceacc60ec5
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-11-08 11:25:11 +0530

    added zipkin

--- jobMicroservice/src/main/java/com/ritikjha/jobMicroservice/client/CompanyClient.java
+++ jobMicroservice/src/main/java/com/ritikjha/jobMicroservice/client/CompanyClient.java
@@ -1,4 +1,8 @@
 package com.ritikjha.jobMicroservice.client;
 
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "company-service")
 public interface CompanyClient {
 }
